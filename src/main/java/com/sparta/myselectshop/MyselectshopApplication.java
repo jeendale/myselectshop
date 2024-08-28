2ac94356d163c310890351c59bcc6cf274dc9173
jeendale <jeekoo7203@gmail.com>
2024-08-29 00:40:21 +0900

    Controller테스트 - 복잡하다 Jpaconfig도 뺴준다

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -2,11 +2,10 @@ package com.sparta.myselectshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@EnableJpaAuditing
+//@EnableJpaAuditing controller테스트를 위해 jpaconfig로 이동
 @SpringBootApplication
 public class MyselectshopApplication {
 
